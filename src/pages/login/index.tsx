e9c17010dbe5e2d41b4cad54703c66780e82a33e
Erick <erickkan2004@gmail.com>
2024-08-26 19:50:19 -0300

    Creating login structure

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { 
+    Text, 
+    View
+} from 'react-native';
+
+import { style } from "./style";
+
+
+export default function Login(){
+    return (
+        <View style={style.container}>
+            <View style={style.boxTop}>
+                <Text>Top</Text>
+            </View>
+
+            <View style={style.boxMid}>
+                <Text>Mid</Text>
+            </View>
+            
+            <View style={style.boxBottom}>
+                <Text>Bottom</Text>
+            </View>
+        </View>
+    )
+}
